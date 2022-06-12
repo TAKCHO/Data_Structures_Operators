c8c8634000a387e0754b589fecdd6be7b005378c
Tako <takogiorgobiani97@gmail.com>
2022-06-12 14:10:02 +0400

    optional chaining

--- script.js
+++ script.js
@@ -54,6 +54,40 @@ const restaurant = {
   },
 };
 
+//////////////////////////////////////
+// optional chanining
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+/// With optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`on ${day}, we open at ${open}`);
+}
+
+//methods
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+//Arrays
+
+const users = [
+  {
+    name: 'Tako',
+    email: 'hello@tako.io',
+  },
+];
+
+console.log(users[0]?.name ?? 'User array empty');
+
 ///////////////////////////////////////////
 //////Looping Arrays: The for-of loop
 /* 
