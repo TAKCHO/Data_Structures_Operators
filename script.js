7fe9a11cbc0eda0940d661935111dc9c38dac87d
Tako <takogiorgobiani97@gmail.com>
2022-06-22 23:52:28 +0400

    working with strings  - slice

--- script.js
+++ script.js
@@ -67,6 +67,19 @@ console.log('B737'[0]);
 console.log(airline.length);
 console.log('B737'.length);
 
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
 /* // /////////////////// MAPS PT2
 
 const question = new Map([
