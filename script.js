921a051e0c7d078196d4d0ec9ccb427697087afa
Tako <takogiorgobiani97@gmail.com>
2022-06-23 23:59:19 +0400

    strings - booleand

--- script.js
+++ script.js
@@ -90,7 +90,32 @@ console.log(priceUS);
 const announcement =
   'All passengers come to boarding door 23. Boarding door 23';
 console.log(announcement.replace('door', 'gate'));
-console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate')); // console.lo(announcement.replace(/door/g, "gate"));
+
+//booleans
+
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+//pracite exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks anda camera');
+checkBaggage('Got some snacks and a gun for protection');
 
 /// working with strings - part 1
 /* const airline = 'TAP Air Portugal';
